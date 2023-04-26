bcd566e59b22e2488031dc7723218929df75a612
Hussein Ibrahim <husseinib23920@gmail.com>
2023-04-26 23:41:02 +0300

    Update database seeders to generate randomly data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,65 +17,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        User::truncate();
-//        Post::truncate();
-//        Category::truncate();
-
-        $user = User::factory()->create([
-            'name' => 'John Doe'
-        ]);
-
-        Post::factory(5)->create([
-            'user_id' => $user->id
-        ]);
-
-//
-//        $personal = Category::create([
-//            'name' => 'Personal',
-//            'slug' => 'personal'
-//        ]);
-//
-//        $family = Category::create([
-//            'name' => 'Family',
-//            'slug' => 'family'
-//        ]);
-//
-//        $work = Category::create([
-//            'name' => 'Work',
-//            'slug' => 'work'
-//        ]);
-//
-//        Post::create([
-//            'category_id' => $family->id,
-//           'user_id' => $user->id,
-//           'title' => 'My Family Post',
-//           'excerpt' => 'Avada Kadavra',
-//           'slug' => 'my-first-post',
-//           'body' => '<p>Berrr shribibibdopp diopppd odp yess yess eysss</p>',
-//       ]);
-//
-//        Post::create([
-//            'category_id' => $work->id,
-//           'user_id' => $user->id,
-//           'title' => 'My Work Post',
-//           'excerpt' => 'Avada Kadavra',
-//           'slug' => 'my-second-post',
-//           'body' => '<p>Berrr shribibibdopp diopppd odp yess yess eysss</p>',
-//       ]);
-//
-//        Post::create([
-//            'category_id' => $personal->id,
-//           'user_id' => $user->id,
-//           'title' => 'My Personal Post',
-//           'excerpt' => 'Avada Kadavra',
-//           'slug' => 'my-third-post',
-//           'body' => '<p>Berrr shribibibdopp diopppd odp yess yess eysss</p>',
-//       ]);
-//
-//
-//        \App\Models\User::factory()->create([
-//            'name' => 'Test User',
-//            'email' => 'test@example.com',
-//        ]);
+        Post::factory(6)->create();
     }
 }
